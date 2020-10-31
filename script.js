3968e463ba901643068ec351c56c25f036f7096b
Speloquin <speloqu24@gmail.com>
2020-10-31 15:33:20 -0600

    Five Day display

--- script.js
+++ script.js
@@ -15,26 +15,33 @@ $(document).ready(function() {
             // div everything is located in 
             var currentCity = $("#current-display");
             var currentCityName = $("<h1>");
-            var currentTemp = $("<h4>");
-            var feelsLike = $("<h4>");
-            var description = $("<p>")
+            var currentTemp = $("<p>");
+            var feelsLike = $("<p>");
+            var humidity = $ ("<p>");
+            var description = $("<h5>");
+            var wind = $("<p>");
             var currentIcon = $("<img>")
             var weatherIcon = data.weather[0].icon;
             var icons = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
-            // assign the text
-
+            
+        
+        
         currentIcon.attr("src", icons);
 
         currentCityName.text(data.name)
-        currentTemp.text("Actual: " + (data.main.temp));
-        feelsLike.text("Feels Like: " + (data.main.feels_like));
+        currentTemp.text("Actual: " + (data.main.temp) + "°F");
+        feelsLike.text("Feels Like: " + (data.main.feels_like) + "°F");
         description.text(data.weather[0].description)
+        humidity.text("Humidity: " + (data.main.humidity) + "%");
+        wind.text("Wind Speed: " + (data.wind.speed));
 
         currentCity.append(currentCityName)
         currentCity.append(currentIcon)
         currentCity.append(description)
         currentCity.append(currentTemp)
         currentCity.append(feelsLike)
+        currentCity.append(humidity)
+        currentCity.append(wind)
 
         })
 
@@ -46,18 +53,42 @@ $(document).ready(function() {
         }).then (function (data){
             console.log(data)
         
-        var cardDiv = $("#card-display");    
-            
+        // var cardDiv = $("#card-display");  
+        
+        $(".forecastDisplay").empty();
+
+        $(".display").append(`
+        <h3> Five Day Forecast: </h3>
+        `)
+
+        
+        
         for (var i = 0; i < data.list.length; i+=8) {
-            var cardContext = 
-                    // var temp = document.createElement("p")
+            
+            var date = data.list[i].dt_txt;
+            var temp = data.list[i].main.temp + "°F"
+            var forecastIcon = data.list[i].weather[0].icon;
+            var fiveDayIcons = "http://openweathermap.org/img/w/" + forecastIcon + ".png";
+            console.log(fiveDayIcons)
+
+            $(".forecastDisplay").append(`
+            <div class= "col mb-4">
+                <div class= "card">${date}
+                <img src = "${fiveDayIcons}" width="50";>
+                <p>${temp}</p>
+                </div>
+            </div>
+            `)
+
+            // var cardContext = 
+            //         // var temp = document.createElement("p")
             
 
-            date.textContent = data.list[i].dt_txt;
-            console.log(data.list[i].dt_txt)
-                    // temp.textContent = data.list[i].main.temp;
-            fiveDayCard.append(date)
-                    // fiveDayCard.append(temp)
+            // date.textContent = data.list[i].dt_txt;
+            // console.log(data.list[i].dt_txt)
+            //         // temp.textContent = data.list[i].main.temp;
+            // fiveDayCard.append(date)
+            //         // fiveDayCard.append(temp)
 
 
                 }
@@ -80,10 +111,6 @@ $(document).ready(function() {
 
 
 // }
-
-
-
-
 
 
 
